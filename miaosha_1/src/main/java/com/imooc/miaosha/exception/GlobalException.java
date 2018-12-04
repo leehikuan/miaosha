6d9506b662950960ffd713bcb8eb454ba09190f6
lihaikuan <33143019+lihaikuan@users.noreply.github.com>
2018-12-04 11:58:51 +0800

    异常处理初次提交

---
+++ miaosha_1/src/main/java/com/imooc/miaosha/exception/GlobalException.java
@@ -0,0 +1,16 @@
+package com.imooc.miaosha.exception;
+
+import com.imooc.miaosha.result.CodeMsg;
+
+public class GlobalException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	private CodeMsg cm;
+	public GlobalException(CodeMsg cm) {
+		this.cm=cm;
+	}
+	public CodeMsg getCm() {
+		return cm;
+	}
+	
+}
